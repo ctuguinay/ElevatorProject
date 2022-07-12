c07f2e5848ccb45a2ffb2e4ef5a18104ff2c79cc
ctuguinay <ctuguina@uw.edu>
2022-07-11 18:37:37 -0700

    add argparser

--- visualizations/view_data_dist.py
+++ visualizations/view_data_dist.py
@@ -1,10 +1,23 @@
 import os
 import os.path
 import numpy as np
+import argparse
 from numpy import genfromtxt
 from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
+
+    # Parser to parse this script's arguments that pertain to our generated data.
+    parser = argparse.ArgumentParser(description='Argument parser for viewing the distribution of our dataset.')
+    parser.add_argument("--set_width", type=int,
+                        help="Sets with of our Maplotlib Canvas.",
+                        default=15)
+    parser.add_argument("--set_height", type=int,
+                        help="Sets height of our Maplotlib Canvas.",
+                        default=15)
+    args = parser.parse_args()
+    width = args.set_width
+    height = args.set_height
     
     # Get absolute current path.
     cur_path = os.path.dirname(__file__)
@@ -21,9 +34,9 @@ if __name__ == "__main__":
     # Set subplots.
     figure, axis = plt.subplots(2, 2)
 
-    # Set figure height and width.
-    figure.set_figheight(15)
-    figure.set_figwidth(15)
+    # Set figure width and height.
+    figure.set_figwidth(width)
+    figure.set_figheight(height)
 
     # Get numpy array in seconds.
     seconds_np = data_np[:, 1]
