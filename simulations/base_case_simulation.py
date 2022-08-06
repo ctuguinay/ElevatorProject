222bc18d84e3eb2fcbf75e9f2f6faedd06b10ddf
jeffrey <ziyuw5@uw.edu>
2022-08-06 16:29:01 -0700

    update added_time as list

--- simulations/base_case_simulation.py
+++ simulations/base_case_simulation.py
@@ -99,7 +99,7 @@ def useState(timelist: TimeList, current_state: State, current_event: TimeListEv
     # across all hall calls) whenever people get to their destination. added_time will be the amount by 
     # which we increment the total time waited by as a result of people getting to their destination during this event,
     # which will sometimes happen on Arrival events
-    added_time = 0
+    added_time = []
 
     if not timelist.has_next() and current_event.object_type != "Hall Call" and current_state.elevator.total_passenger_weight() == 0:
         # there are no hall calls left and nobody's in the elevator. Our work is done, so we return the empty
@@ -227,7 +227,8 @@ def useState(timelist: TimeList, current_state: State, current_event: TimeListEv
     
 
         for person in removed:
-            added_time += current_state.time - person.time
+            #added_time += current_state.time - person.time
+            added_time.append(current_state.time - person.time)
 
         if command.going_up:
             calls = current_state.up_calls
@@ -300,7 +301,7 @@ def state_to_elevator_input(state:State) -> Tuple[int,
 if __name__ == "__main__":
 
     # Initialize Total Time for all passengers that takes into account wait time and travel time.
-    total_time = 0
+    total_time = []
 
     # Get csv path.
     script_dir = os.path.dirname(__file__)
@@ -332,7 +333,11 @@ if __name__ == "__main__":
     while timelist.has_next():
         current_event = timelist.next_event()
         timelist, result_state, added_time = useState(timelist, current_state, current_event, model)
-        total_time = total_time + added_time
+        #total_time = total_time + added_time
+        total_time_temp = 0
+        for i in len(added_time):
+            total_time_temp = total_time_temp + added_time[i]
+            total_time.append(total_time_temp)
         current_state = result_state
 
     # Prints out the total time.
