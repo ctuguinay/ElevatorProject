4c51bdafc79d40a96388b17820ed14bc5a9563de
Caesar Tuguinay <cmtuguinay2000@gmail.com>
2022-10-14 11:36:15 -0700

    add tests to base case simulation

--- simulations/tests/test_base_case_simulation.py
+++ simulations/tests/test_base_case_simulation.py
@@ -5,11 +5,22 @@ from simulations.classes.Log import Log, LogPIT
 from simulations.classes.TimeList import TimeList, TimeListEvent
 from simulations.classes.State import State
 from simulations.classes.Elevator import Elevator
-from simulations.base_case_simulation import load_timelist, initialize_values, state_to_elevator_input
+from simulations.classes.Model import Model
+from simulations.base_case_simulation import argparse_create, load_timelist, initialize_values, state_to_elevator_input
+
+def test_argparse_create():
+    """
+    Tests is argparse_create has the desired behavior.
+    """
+
+    parsed_args = argparse_create(["--python_command", "py"])
+    python_command = parsed_args.python_command
+
+    assert python_command == "py"
 
 def test_load_timelist():
     """
-    Tests if load_timelist works.
+    Tests if load_timelist has the desired behavior.
     """
 
     # Get csv path.
@@ -33,12 +44,79 @@ def test_load_timelist():
     assert hall_call.dest_floor == 3
     assert hall_call.weight == 93.84930521474159
 
-# TODO: def test_initialize_values():
+@pytest.fixture
+def preload_timelist():
+    """
+    Preloads the timelist after we know that load_timelist works as it should.
+    """
+
+    # Get csv path.
+    script_dir = os.path.dirname(__file__)
+    full_path = os.path.join(script_dir, "data/CSVs/data_test.csv").replace("simulations\\tests\\", "")
+
+    # Initialize Timelist with reader.
+    timelist = TimeList()
+    with open(full_path, mode='r', newline='') as file:
+        reader = csv.reader(file, delimiter=',', quotechar='"')
+        timelist = load_timelist(reader,timelist)
+
+    return timelist
+
+def test_initialize_values(preload_timelist):
     """
     Test initialize_values for proper behavior.
     """
 
-# TODO: def test_state_to_elevator_input():
+    timelist = preload_timelist
+    assert isinstance(timelist, TimeList)
+
+    _, result_timelist, _, _, _, _ = initialize_values(timelist, -1)
+
+    assert result_timelist == timelist
+    assert len(result_timelist.future) == 2
+
+    for event in result_timelist.future:
+        assert isinstance(event, TimeListEvent)
+
+    total_time, result_timelist, elevator, current_state, log, model = initialize_values(timelist, 1)
+
+    assert isinstance(total_time, list)
+    assert isinstance(result_timelist, TimeList)
+    assert isinstance(elevator, Elevator)
+    assert isinstance(current_state, State)
+    assert isinstance(log, Log)
+    assert isinstance(model, Model)
+
+    assert len(result_timelist.future) == 1
+
+    for event in result_timelist.future:
+        assert isinstance(event, TimeListEvent)
+
+    log_pit = log.next_log_pit()
+    assert isinstance(log_pit, LogPIT)
+
+def test_state_to_elevator_input(preload_timelist):
     """
     Test state_to_elevator_input for proper behavior.
     """
+
+    timelist = preload_timelist
+    _, _, _, current_state, _, _ = initialize_values(timelist, -1)
+
+    capacity, curr_weight, curr_pos, buttons_pressed, up_buttons, down_buttons = state_to_elevator_input(current_state)
+
+    assert isinstance(capacity, float)
+    assert isinstance(curr_weight, float) or isinstance(curr_weight, int)
+    assert isinstance(curr_pos, int) 
+    assert isinstance(buttons_pressed, dict)
+    assert isinstance(up_buttons, dict)
+    assert isinstance(down_buttons, dict)
+
+    for item in buttons_pressed.items():
+        assert item[1] == False
+
+    for item in up_buttons.items():
+        assert item[1] == False
+    
+    for item in down_buttons.items():
+        assert item[1] == False
