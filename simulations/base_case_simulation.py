c1c3a7ba7ad2f12178ac9ce156a49479d85c4fd1
Caesar Tuguinay <cmtuguinay2000@gmail.com>
2022-10-14 11:52:05 -0700

    clean up base_case_simulation

--- simulations/base_case_simulation.py
+++ simulations/base_case_simulation.py
@@ -35,7 +35,7 @@ HEIGHT = {value:key for value in range(1, NUM_FLOORS + 1) for key in [(value - 1
     # the average floor-to-floor height is 4.65 (here glossed as 4.5) m in an office building). Theoretically,
     # this can be altered to create variable distances in the case of a specific building.
 
-# Number of times the simulation is run for each number of people
+# Number of times the simulation is run for each number of people sampled.
 RUNS = 50
 
 def argparse_create(args):
@@ -70,10 +70,6 @@ def load_timelist(reader, timelist):
 
     Returns:
         timelist: TimeList object filled with TimeListEvents containing information from the reader.
-    """
-
-    """
-    WHAT NEEDS TO BE DONE: Use reader somewhere here to load the timelist object.
     """
 
     for row in reader:
@@ -137,67 +133,61 @@ def useState(timelist: TimeList, current_state: State, current_event: TimeListEv
     # We update the total time waited (which is the sum of (<time arrived at dest> - <time when started waiting>) 
     # across all hall calls) whenever people get to their destination. added_time will be the amount by 
     # which we increment the total time waited by as a result of people getting to their destination during this event,
-    # which will sometimes happen on Arrival events
+    # which will sometimes happen on Arrival events.
     added_time = []
 
     if not timelist.has_next() and current_event.object_type != "Hall Call" and current_state.elevator.total_passenger_weight() == 0:
         # there are no hall calls left and nobody's in the elevator. Our work is done, so we return the empty
-        # timelist and exit out of the while loop in main
+        # timelist and exit out of the while loop in main.
         return timelist, current_state, added_time
 
     # The first thing we do is modify the attributes that can be modified without knowing the model's
     # output (which is the command telling us the next thing to do).
-    # For example, we advance the state's time to the time of this event
+    # For example, we advance the state's time to the time of this event.
     current_state.time = current_event.time
 
     
     if current_event.object_type == "Arrival":
         # Things that are specific to arrivals, like setting current_state.elevator.moving to false and
+        # setting the floor that the elevator currently is on.
         current_state.elevator.moving = False
         current_state.elevator.current_floor = current_event.object.floor
     
     elif current_event.object_type == "Hall Call":
-        # Things that are specific to Hall Calls, like adding the people the up_calls or down_calls lists
+        # Things that are specific to Hall Calls, like adding the people on the up_calls or down_calls.
         if current_event.object.dest_floor > current_state.elevator.current_floor:
             current_state.up_calls[current_event.object.start_floor].append(current_event.object)
         else: # destination < current_event.start_floor
             current_state.down_calls[current_event.object.start_floor].append(current_event.object)
 
     elif current_event.object_type == "Door Close":
-        # Things that are specific to Door Close, like setting current_state.elevator.letting_people_in to False
+        # Changes that are specific to Door Close.
         current_state.elevator.letting_people_in = False
         current_state.elevator.going_up = None
         
-    
-
     else:
         raise ValueError("Next event had unexpected type")
     
     if current_state.elevator.moving:
-        # the elevator is currently moving between floors so
-        # we can't do anything. No need to call the Model, just return with the state we've changed
+        # The elevator is currently moving between floors so we can't do anything.
+        # No need to call the Model, just return with the state we've changed.
         return timelist, current_state, added_time
-
 
     if current_state.elevator.letting_people_in:
         # If anyone's on this floor, change state to reflect letting them in to this elevator. We
         # again can't change our behavior, so no need to call the Model, just return with the state we've changed.
         current_state.elevator.moving = False
 
-        # update the passenger
-        # person.behavior == "Hall Call"
-        # Elevator.persons_in_elevator.add(person)
-
         if current_state.elevator.going_up:
-            #let people who have upcalls into elevator
+            # Let people who have up calls into elevator.
             current_state.elevator.add_floor(current_state.up_calls)
         else: # going down
-            #let people who have downcalls into elevator
+            # Let people who have down calls into elevator.
             current_state.elevator.add_floor(current_state.down_calls)
 
         return timelist, current_state, added_time
     
-    # ask the model what to do
+    # Ask the model what to do.
     capacity, curr_weight, curr_pos, buttons_pressed, up_buttons, down_buttons = state_to_elevator_input(current_state)
     command = model.get_command(capacity, curr_weight, curr_pos, buttons_pressed, up_buttons, down_buttons)
 
@@ -207,19 +197,27 @@ def useState(timelist: TimeList, current_state: State, current_event: TimeListEv
     elif type(command) is Move:
         
         def journey_time() -> int:
+            """
+            Calculates the journey time of the elevator for a move command.
+
+            Returns:
+                Time in an int/float value that describes the journey time of the elevator.
+            """
+
             start = current_state.elevator.current_floor
             end = start + 1
 
-            # one possible approach below using sigmoid functions to model the accelerating and
-            # deccelerating periods, solved for the distance traversed
+            # One possible approach below using sigmoid functions to model the accelerating and
+            # deccelerating periods, solved for the distance traversed.
             PERCENT_ACCELERATING = 0.2 # Represents the percentage of time it takes to accelerate to
-                # top speed when going from one floor to the one immediately above.
+            # top speed when going from one floor to the one immediately above.
 
             def variable_if_arrival():
                 """ 
                 Gives the time taken to traverse the floors if the elevator is stopping at the
                 next floor.
                 """
+
                 floors = abs(start - end)
                 return ((((current_state.elevator_speed ** -1) / 4) ** -1) * floors + 
                         5 * PERCENT_ACCELERATING - 0.5 * PERCENT_ACCELERATING * (log1p(e ** 5) - 5)) // 5
@@ -229,11 +227,11 @@ def useState(timelist: TimeList, current_state: State, current_event: TimeListEv
                 Gives the time taken to traverse the floors if the elevator is not stopping at
                 the next floor.
                 """
+
                 floors = abs(start - end)
                 return ((((current_state.elevator_speed ** -1) / 4) ** -1) * floors + 
                         0.5 * PERCENT_ACCELERATING * (log1p(e ** 5) + 5)) // 5 + PERCENT_ACCELERATING
 
-            # Should always return 4
             return variable_if_continuing()
 
         arrival_floor = None
@@ -242,7 +240,7 @@ def useState(timelist: TimeList, current_state: State, current_event: TimeListEv
         else:
             arrival_floor = current_state.elevator.current_floor - 1
         if arrival_floor < 1 or arrival_floor > current_state.elevator.top_floor:
-            # If the model gives us invalid instructions, just idle
+            # If the model gives us invalid instructions, just idle.
             return timelist, current_state, added_time
         arrival_time = current_state.time + journey_time()
         new_event = TimeListEvent(arrival_time, 'Arrival', Arrival(arrival_floor))
@@ -260,7 +258,6 @@ def useState(timelist: TimeList, current_state: State, current_event: TimeListEv
             current_state.elevator.persons_in_elevator[person.start_floor].remove(person)
             current_state.elevator.curr_weight -= person.weight
     
-
         for person in removed:
             added_time.append(current_state.time - person.time)
 
@@ -294,6 +291,7 @@ def state_to_elevator_input(state:State) -> Tuple[float, float, int,
     the up_calls that have been made (dict from floor num to if made), 
     and the down_calls that have been made (dict from floor num to if made)
     """
+
     curr_pos = state.elevator.current_floor
     buttons_pressed = state.elevator.buttons_pressed
     up_buttons = {}
@@ -364,32 +362,47 @@ def initialize_values(full_timelist, number_samples):
     start_log_pit = LogPIT(current_state, timelist, [], [])
     log.add_log_pit(start_log_pit)
 
-    # Initialize Model
+    # Initialize Model.
     model = Model()
 
     return total_time, timelist, elevator, current_state, log, model
 
 def add_to_timelist(timelist, filepath):
+    """
+    Initializes values for the base case simulation.
+
+    Args:
+        timelist: Timelist object with no hall calls.
+        filepath: String that describes the filepath to the timelists.
+
+    Returns:
+        total_time: Array that is the initialized total wait time of all people that left the elevator here.
+        timelist: Timelist object that has been initialized with a sample of hall calls.
+        current_state: State object that has been modified to take into account the most recent event in the simulation.
+        elevator: Elevator object that has been properly initialized.
+        log: Log object to track every part of the simulation.
+        model: Initialized base case model.
+    """
+
     try:
         timelist = TimeList()
         with open(filepath, mode='r', newline='') as file:
             reader = csv.reader(file, delimiter=',', quotechar='"')
             timelist = load_timelist(reader,timelist)
     except PermissionError:
-        # Retry
+        # Retry.
         print("Permission Error excepted")
         return add_to_timelist(timelist, filepath)
     
     return timelist
 
-
 if __name__ == "__main__":
 
     # Get parsed args.
     args = argparse_create((sys.argv[1:]))
-
     python_command = args.python_command
 
+    # Set data generation script path.
     gen_script = "data/create_csv.py"
 
     # Initialize CSV Tracker.
@@ -404,10 +417,13 @@ if __name__ == "__main__":
     for number_people_pre in range(20):
         number_people = (number_people_pre + 1) * 40
 
+        # Initialize empty results array.
         full_run_result = np.zeros(7)
 
+        # Iterate through number of runs.
         for run in tqdm(range(RUNS)):
             
+            # Run data generation script.
             full_gen_path = script_dir.replace("simulations", "") + gen_script
             subprocess.run([python_command, full_gen_path, f"--set_persons={number_people}", f"--set_floors={NUM_FLOORS}"])
 
@@ -419,9 +435,9 @@ if __name__ == "__main__":
 
             # Get initial values.
             total_time, timelist, elevator, current_state, log, model = initialize_values(full_timelist, -1)
+            count=0
 
             # Repeat until no more events in timelist.
-            count=0
             while timelist.has_next():
                 count += 1
                 current_event = timelist.next_event()
@@ -437,25 +453,20 @@ if __name__ == "__main__":
                 log.add_log_pit(current_log_pit)
                 current_state = result_state
 
-            # Prints out the length of total_time
+            # Prints out the length of total_time.
             length_total_time = len(total_time)
-            #print(length_total_time)
 
-            # Prints out mean of total_time
+            # Prints out mean of total_time.
             mean_total_time = mean(total_time)
-            #print(mean_total_time)
 
-            # Prints out median of total_time
+            # Prints out median of total_time.
             median_total_time = median(total_time)
-            #print(median_total_time)
 
-            # Prints out max of total_time
+            # Prints out max of total_time.
             max_total_time = max(total_time)
-            #print(max_total_time)
 
             # Prints out sum of total_time.
             sum_total_time = length_total_time * mean_total_time
-            #print(sum_total_time)
 
             row = np.array([number_people, num_calls, length_total_time, mean_total_time, median_total_time, max_total_time, sum_total_time])
             full_run_result += row
